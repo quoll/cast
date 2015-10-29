b2778e34eb43697da632b8d326d39a4543130b54
Paul Gearon <pgearon@revelytix.com>
2015-10-29 07:36:41 -0400

    getting basic schema in place

--- src/clj/cst/data.clj
+++ src/clj/cst/data.clj
@@ -1,5 +1,37 @@
-(ns cst.data)
+(ns cst.data
+  (:import [datomic Peer]))
 
-(defn datomic-struct
-  [s]
-  )
+(def std-types [:keyword :string :boolean :long :bigint :float :double :bigdec :instant :uuid :uri])
+
+(def types (map #(let [nm (name %)]
+                  {:db/id (Peer/tempid :db.part/db)
+                   :db/ident (keyword "cst" nm)
+                   :db/valueType (keyword "db.type" nm)
+                   :db/cardinality :db.cardinality/one
+                   :db.install/_attribute :db.part/db})
+                std-types))
+
+(def basic-schema
+  [{:db/id (Peer/tempid :db.part/db)
+    :db/ident :cst/type
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db}
+   {:db/id (Peer/tempid :db.part/db)
+    :db/ident :cst/element
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db.install/_attribute :db.part/db}
+   {:db/id (Peer/tempid :db.part/db)
+    :db/ident :cst/index
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db}
+   {:db/id (Peer/tempid :db.part/db)
+    :db/ident :cst.value/object
+    :db/valueType :db.type/object
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db}])
+
+(def schema (concat schema types))
+
--- src/clj/cst/database.clj
+++ src/clj/cst/database.clj
@@ -1,6 +1,14 @@
 (ns cst.database
-  (:require [datomic.api :refer [q] :as d]))
+  (:require [cst.data :as data]
+            [datomic.api :refer [q] :as d]))
 
 (def dburl "datomic:dev://localhost:4334/source")
 
+(defn load-schema
+  [c]
+  (d/transact c data/schema))
 
+(defn database [uri]
+  (when (d/create-database uri)
+    (load-schema uri))
+  (d/connect uri))
