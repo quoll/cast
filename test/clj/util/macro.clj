222c2232d092a9c25e7b9d9bc9da3271ac8adf2e
Paul Gearon <pgearon@revelytix.com>
2015-11-12 13:36:13 -0500

    Utility for testing macro arguments

---
+++ test/clj/util/macro.clj
@@ -0,0 +1,11 @@
+(ns util.macro)
+
+(defmacro assert-args
+  [& pairs]
+  `(do (when-not ~(first pairs)
+         (throw (IllegalArgumentException.
+                  (str (first ~'&form) " requires " ~(second pairs) " in " ~'*ns* ":" (:line (meta ~'&form))))))
+       ~(let [more (nnext pairs)]
+          (when more
+            (list* `assert-args more)))))
+
